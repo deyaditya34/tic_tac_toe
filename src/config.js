2e0a51ecdeed529519b88b18daf6d7f5cfc221de
aditya dey <deyaditya34@gmail.com>
2025-04-27 13:18:13 +0530

    deleted the unnecessary files

--- src/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  SERVER_PORT: process.env.SERVER_PORT,
-  JWT_SECRET: process.env.JWT_SECRET,
-  ACTIVE_GAMES_ID_LIST: process.env.ACTIVE_GAMES_ID_LIST,
-  ACTIVE_PLAYERS_ID_LIST: process.env.ACTIVE_PLAYERS_ID_LIST,
-};
